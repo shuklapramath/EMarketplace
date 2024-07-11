b56d4474caf21c542c96a1cfab142958bbe0159f
PramathShukla164 <pramathshukla275@gmail.com>
2024-07-10 18:36:56 -0600

    Category Page

---
+++ pages/categories.js
@@ -0,0 +1,61 @@
+import Layout from "@/components/Layout";
+import axios from "axios";
+import { set } from "mongoose";
+import {useEffect, useState} from 'react';
+
+export default function Categories(){
+    const [name, setName] = useState('');
+    const [parentCategory, setParentCategory] = useState('');
+    const [categories, setCategories] = useState([]);
+    useEffect(() => {
+        fetchCategories();
+    }, [])
+    function fetchCategories(){
+        axios.get('/api/categories').then(result => {
+            setCategories(result.data);
+        });
+    }
+    async function saveCategory(ev){
+        ev.preventDefault();
+        await axios.post('/api/categories', {name});
+        setName('');
+    }
+    return(
+        <Layout>
+            <h1>Categories</h1>
+            <label>New Category name</label>
+            <form onSubmit={saveCategory} className="flex gap-1">
+                <input className="mb-0" 
+                type="text" 
+                placeholder={'Category name'}
+                onChange={ev => setName(ev.target.value)}
+                value={name}/>
+                <select className="mb-0" 
+                        onChange={ev => setParentCategory(ev.target.value)}
+                        value={parentCategory}>
+                    <option value = ""> No parent Category</option>
+                    {categories.length > 0 && categories.map
+                        (category => (
+                            <option value={category._id}>{category.name}</option>
+                        ))
+                    }
+                </select>
+                <button type = "submit" className="btn-primary py-1">Save</button>
+            </form>
+            <table className="basic mt-4">
+                <thead>
+                    <tr>Category Name</tr>
+                </thead>
+                <tbody>
+                    {categories.length > 0 && categories.map(
+                        (category => (
+                            <tr>
+                                <td>{category.name}</td>
+                            </tr>
+                        ))
+                    )}
+                </tbody>
+            </table>
+        </Layout>
+    );
+}
