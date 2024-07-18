a517d52730a4b3bc96df6eb684298b2a4612ca1b
PramathShukla164 <pramathshukla275@gmail.com>
2024-07-18 12:35:55 -0600

    New features

--- components/ProductForm.js
+++ components/ProductForm.js
@@ -98,15 +98,18 @@ export default function ProductForm({
                    ))}
                 </select>
                 {propertiesToFill.length > 0 && propertiesToFill.map(p => (
-                    <div className="flex gap-1">
-                        <div>{p.name}</div>
-                        <select value={productProperties[p.name]}
-                            onChange={ev => 
-                                setProductProp(p.name,ev.target.value)}>
-                            {p.values.map(v => (
-                                <option value = {v}>{v}</option>
-                            ))}
-                        </select>
+                    <div className="">
+                        <label>{p.name[0].toUpperCase()+p.name.substring(1)}</label>
+                        <div>
+                            <select value={productProperties[p.name]}
+                                onChange={ev => 
+                                    setProductProp(p.name,ev.target.value)}>
+                                {p.values.map(v => (
+                                    <option value = {v}>{v}</option>
+                                ))}
+                            </select>
+                        </div>
+                  
                     </div>
                 ))}
                 <label>
@@ -118,7 +121,7 @@ export default function ProductForm({
                     className="flex flex-wrap gap-1"
                     setList={updateImagesOrder}>
                     {!!images?.length && images.map(link => (
-                        <div key={link} className="h-24">
+                        <div key={link} className="h-24 bg-white p-4 shadow-sm rounded-sm border border-gray-200">
                             <img src={link} alt="" className="rounded-lg"/>
                         </div>
                     ))}
@@ -128,11 +131,11 @@ export default function ProductForm({
                             <Spinner />
                         </div>
                     )}
-                    <label className="w-24 h-24 cursor-pointer border text-center flex flex-col items-center justify-center text-grey-500 rounded-lg bg-gray-200">
+                    <label className="w-24 h-24 cursor-pointer text-center flex flex-col items-center justify-center text-sm gap-1 text-primary rounded-sm bg-white shadow-sm border border-primary">
                     <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-6">
                         <path strokeLinecap="round" strokeLinejoin="round" d="M3 16.5v2.25A2.25 2.25 0 0 0 5.25 21h13.5A2.25 2.25 0 0 0 21 18.75V16.5m-13.5-9L12 3m0 0 4.5 4.5M12 3v13.5" />
                     </svg>
-                    <div>upload</div>  
+                    <div>Add Image</div>  
                     <input type="file" onChange={uploadImages} className="hidden"/>             
                     </label>
                 </div>
