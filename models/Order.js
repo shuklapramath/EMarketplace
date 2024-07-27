feba91d063d56d636575d9b5e46886c029aa49d7
PramathShukla164 <pramathshukla275@gmail.com>
2024-07-26 19:02:40 -0600

    admin updated

---
+++ models/Order.js
@@ -0,0 +1,16 @@
+import { model, models, Schema } from "mongoose";
+
+const OrderSchema = new Schema({
+    line_items: Object,
+    name:String,
+    email:String,
+    streetAddress:String,
+    city:String,
+    postalCode:String,
+    country:String,
+    paid:Boolean,
+}, {
+    timestamps: true,
+});
+
+export const Order = models?.Order || model('Order', OrderSchema);
