6ba5a87de3e77fd4474d2654597206c328b8b8b5
PramathShukla164 <pramathshukla275@gmail.com>
2024-11-05 00:21:45 -0700

    updated secret

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -7,7 +7,7 @@ import GoogleProvider from 'next-auth/providers/google'
 const adminEmails = ['pramathshukla275@gmail.com'];
 
 export const authOptions = {
-  secret: process.env.SECRET,
+  //secret: process.env.SECRET,
   providers: [
     GoogleProvider({
       clientId: process.env.GOOGLE_ID,
